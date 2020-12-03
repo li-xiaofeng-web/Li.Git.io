03d5d0cf24070c0ff2f896a2eb79b3816610a52b
li-xiaofeng-web <13190760326@163.com>
2020-12-03 18:31:06 +0800

    初始化

---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+// $('.menu-item').click((event) => {     // header 切换选中状态并跳转页面
+//     $('.menu-item').removeClass('active'); 
+//     $(event.target).addClass('active');
+//     console.log($(event.target).attr('id'));
+// })
